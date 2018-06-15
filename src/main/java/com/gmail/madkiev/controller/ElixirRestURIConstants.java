a4f9019f18ceb7875fbd22f2ff1042da57e1d0da
Shevchenko3141 <MadKiev@gmail.com>
2018-06-15 20:18:50 +0300

    first step

---
+++ src/main/java/com/gmail/madkiev/controller/ElixirRestURIConstants.java
@@ -0,0 +1,15 @@
+package com.gmail.madkiev.controller;
+
+public class ElixirRestURIConstants {
+
+	public static final String TEST_ELIXIR = "/rest/elixir/Elixir of health";
+	public static final String GET_ELIXIR = "/rest/elixir/{id}";
+	public static final String GET_ALL_ELIXIRS = "/rest/elixirs";
+	public static final String CREATE_ELIXIR = "/rest/elixir/create";
+	public static final String DELETE_ELIXIR = "/rest/elixir/delete/{id}";
+	public static final String INPUT_ELIXIR = "/rest/elixir/input";
+	public static final String PROCESS_ELIXIR = "/rest/elixir/process";
+	public static final String OUTPUT_IN_ML_ELIXIR = "/rest/elixir/output";
+	public static final String OUTPUT_IN_DROPS_ELIXIR = "/rest/elixir/output";
+}
+
---
+++ src/main/java/com/gmail/madkiev/model/Elixir.java
@@ -0,0 +1,112 @@
+package com.gmail.madkiev.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+
+public class Elixir implements Serializable{
+
+	private static final long serialVersionUID = -7788619177798333712L;
+	//input
+	private double currentVolume;
+	private double aromaVolume;
+	private double necessaryVolumeOfBottle;
+	//process
+	private double volumeInMl = aromaVolume/currentVolume;
+	private int drops = 33;
+	//outPut
+	private double necessaryAroma = volumeInMl * necessaryVolumeOfBottle;
+	private double dropsOfAroma = necessaryAroma * drops ;
+
+	private int id;
+	private String name;
+	private Date createdDate;
+
+	public static long getSerialVersionUID() {
+		return serialVersionUID;
+	}
+
+	public double getCurrentVolume() {
+		return currentVolume;
+	}
+
+	public void setCurrentVolume(double currentVolume) {
+		this.currentVolume = currentVolume;
+	}
+
+	public double getAromaVolume() {
+		return aromaVolume;
+	}
+
+	public void setAromaVolume(double aromaVolume) {
+		this.aromaVolume = aromaVolume;
+	}
+
+	public double getNecessaryVolumeOfBottle() {
+		return necessaryVolumeOfBottle;
+	}
+
+	public void setNecessaryVolumeOfBottle(double necessaryVolumeOfBottle) {
+		this.necessaryVolumeOfBottle = necessaryVolumeOfBottle;
+	}
+
+	public double getVolumeInMl() {
+		return volumeInMl;
+	}
+
+	public void setVolumeInMl(double volumeOnMl) {
+		this.volumeInMl = volumeOnMl;
+	}
+
+	public int getDrops() {
+		return drops;
+	}
+
+	public void setDrops(int drops) {
+		this.drops = drops;
+	}
+
+	public double getNecessaryAroma() {
+		return necessaryAroma;
+	}
+
+	public void setNecessaryAroma(double necessaryAroma) {
+		this.necessaryAroma = necessaryAroma;
+	}
+
+	public double getDropsOfAroma() {
+		return dropsOfAroma;
+	}
+
+	public void setDropsOfAroma(double dropsOfAroma) {
+		this.dropsOfAroma = dropsOfAroma;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@JsonSerialize(using=DateSerializer.class)
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	
+	
+}
