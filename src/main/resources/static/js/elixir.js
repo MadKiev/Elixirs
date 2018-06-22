d499ac1272b9a369a283802a94d7ca642c4d47b8
Maksym Veselov <maksym.veselov@harmonicinc.com>
2018-06-22 20:13:40 +0300

    add Elixir form

--- src/main/resources/static/js/elixir.js
+++ src/main/resources/static/js/elixir.js
@@ -5,5 +5,23 @@ angular.module('elixir', ['ngResource'])
     }])
     .controller('ElixirController',['$resource', function($resource) {
         var scope = this;
-        scope.items = $resource('/api/v1/ingredients').query();
+        scope.elixirs = $resource('/api/v1/elixirs').query();
+        scope.selected = {};
+
+        scope.recipes = [];
+
+        scope.addRecipes = function() {
+            scope.recipes.push({id:scope.selected.id, ingredient:scope.selected.ingredient, quantity:scope.selected.quantity});
+            scope.selected = {};
+        };
+
+        scope.items = [{
+            id: 1,
+            ingredient: 'aLabel',
+            quantity: 10
+        }, {
+            id: 2,
+            ingredient: 'bLabel',
+            quantity: 15
+        }];
     }]);
